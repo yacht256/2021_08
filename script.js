43a52c399aadfd7fb80da40af4195826917ad298
yacht2562 <Kasira_k@cmu.ac.th>
2021-06-08 14:47:47 +0700

    V.3
    add this class so easy

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
-var newText = 'new Text'
-document.getElementById('ok').innerHTML = newText
+
+document.getElementById('ok').addEventListener('click',function(e){
+    let noElement = document.getElementById('no')
+    if (noElement.classList.contains('toggleOn')){
+        noElement.classList.replace('toggleOn','toggleOff')
+    }else if(noElement.classList.contains('toggleOff')){
+        noElement.classList.replace('toggleOff','toggleOn')
+    }else{
+        noElement.classList.add('toogleOn')
+    }
+})
+
+var borderTick =1
+document.getElementById('cancel').style.border = `${borderTick}px solid white`
+var cancelElement =document.getElementById('cancel')
+cancelElement.addEventListener('mouseover', function(e){
+    cancelElement.innerHTML =`
+    <div class="container-col">
+        <div class="flex-item">
+        no 1 </div>
+        <div class = "flex-item">
+            no 2 
+        </div>
+        </div>
+        `
+})
+cancelElement.addEventListener('mouseleave',function (e){
+    cancelElement.innerHTML = `cancel`
+})
+var addMargin = document.getElementById('ok')
+var Margin_add = 1;
+
+addMargin.addEventListener('click', function (e){
+  
+       Margin_add+=3
+       document.getElementById('cancel').style.margin= `0px ${Margin_add}px `
+       document.getElementById('no').style.margin= `0px ${Margin_add}px `
+      
+    
+   
+})
+
+var dbChange = document.getElementById('no')
+dbChange.addEventListener('dblclick',function (e){
+    addMargin.innerHTML = `<div id="ok" class="flex-item">ok, วิชานี้ so easy</div>`
+}) 
+
+
